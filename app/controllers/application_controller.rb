1f16d54ec15888b0fa3a6d92a9465a67bbf4f327
Hiroki Yanase <60053865+YanaseHiroki@users.noreply.github.com>
2020-09-05 19:10:44 +0900

    トップページのCSSを改善

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # アカウント編集の時にnameとimageのストロングパラメータを追加
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
+  end
+
 end
