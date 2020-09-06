f7912ce3d887a95531675ef8559107aca5701302
Hiroki Yanase <60053865+YanaseHiroki@users.noreply.github.com>
2020-09-06 21:04:06 +0900

    計画を作成・編集・削除する機能の実装

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,10 +1,63 @@
 class PlansController < ApplicationController
+  before_action :find_plan, only: [:show, :edit, :update, :destroy]
+
   def index
+    @plans = Plan.all
   end
 
   def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    if @plan.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
+    # logをすべてとってくる
+    @plans = Plan.all
+    @ratios = ['◎', '◎', '◎', '◎', '◎', '◎', '◎']
+    @point = 100
   end
+
+  def edit
+    @plan = Plan.find(params[:id])
+    redirect_to root_path if @plan.invalid?
+  end
+  
+  def update
+    if @plan.update(plan_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @plan.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
+  def find_plan
+    @plan = Plan.find(params[:id])
+  end
+  
+  private
+
+  def plan_params
+    params.require(:plan).permit(
+      :target,
+      :trigger,
+      :habit
+    )
+  end
+
 end
